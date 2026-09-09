a7db46e44d511d2e7d8793e84ce91a8f77d13799
AnkitHeree <ankityadav9082384811@gmail.com>
2026-09-09 16:51:41 +0530

    Updated css

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
+  
 })
 export class App {
   protected readonly title = signal('my-angular-app');
